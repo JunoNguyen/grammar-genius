15d679b8355f40cf47bddbde1ff007c0b2ecb53c
Juno <junonguyen27@gmail.com>
2022-02-22 19:19:49 -0600

    added install logic

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -20,7 +20,7 @@ export const putDb = async (content) => {
   const store = tx.objectStore('jate');
   const request = store.put({ id: id, todo: content });
   const result = await request;
-  console.error('putDb not implemented');
+  // console.error('putDb not implemented');
 };
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
